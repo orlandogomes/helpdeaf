f5f008cd288a98cd89d4a72d66152e3b9093fc3c
orlandogomes <orlandog.mariano@gmail.com>
2016-02-08 12:19:25 +0000

    Alterações layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,71 @@
 angular.module('app.controllers', [])
   
-.controller('loginCtrl', function($scope) {
+.controller('loginCtrl', function($scope, $http, getAPIBaseUrl) {
 
-})
+ //Testes Controllers
+ 
+  
+    $scope.enviarForm = function(usuarioForm, usuario) {
+    if (usuarioForm.$valid) {
+      postarAPI(usuario);
+    }
+  };
+  
+      var postarAPI = function(usuario) {
+        // Simple GET request example:
+        var url = getAPIBaseUrl() + "/inserejson";
+        var dados = {
+          "dados": usuario,
+          "colecao": "Usuarios"
+        };
+        console.log(dados);
+        $http({
+          url: url,
+          method: 'POST',
+          data: dados,
+          headers: {
+            'content-type': 'application/json'
+          }
+        });
+        $scope.buscaAPI();
+      };
+    
+       
+         
+          $scope.buscafour = function(param){
+            
+            //vars da url
+            var url = "";
+            var key = "";
+            var par = window.encodeURIComponent(param);
+            //concatenacao da url       
+            var url_get = url + key
+            //execucao da url
+            $http.get(url_get).then(function successCallback(response) {
+                console.log(response);
+                $scope.result = response.data.response.venues;
+                //console.log(response.venues);
+              }, function errorCallback(response) {
+                alert(JSON.stringify(response));
+            });
+        };
+       
+    
+    })
    
 .controller('cadastroGeralCtrl', function($scope) {
 
+
 })
    
-.controller('configuraçõEsCtrl', function($scope) {
+.controller('configuraçõesCtrl', function($scope) {
+    
 
 })
    
 .controller('perfilSurdoCtrl', function($scope) {
-
+    
+    
 })
    
 .controller('perfilOuvinteCtrl', function($scope) {
